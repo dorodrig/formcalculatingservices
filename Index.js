2811d0ea9c6d3f0986ca6088852c1399f000220c
dorodrig <dorodriguez145@misena.edu.co>
2023-09-29 22:27:52 -0500

    first commit project free

---
+++ Index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'formcalculatingservices')));
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, 'style.css'));
+  });
+  
+app.get('/', (req, res) => {
+    const html = ` 
+    <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home FA</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+
+  <body>
+    <div class="main_title">
+      <h1 class="title">CALCULARO DE SERVICIOS PUBLICOS</h1>
+    </div>
+    <div class="containerluz">
+      <div class="container1">
+        <font size="10px" class="title1">Servicio de luz</font>
+      </div>
+      <div class="container2">
+        <font size="5px" class="container1title1">Ingrese valor $</font>
+        <!-- <label size="9px" for="CC_CAMPO1">Ingrese valor</label> -->
+        <br /><input
+          type="number"
+          id="CC_CAMPO1"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar solo el valor numerico</font
+        >
+      </div>
+      <div class="container3">
+        <font size="5px" class="container1title1">Ingrese los kWh</font>
+        <br /><input
+          type="number"
+          id="CC_CAMPO2"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar numeros
+        </font>
+      </div>
+    </div>
+    <div class="containergas">
+      <div class="container4">
+        <font size="10px" class="title1">Servicio de gas</font>
+      </div>
+      <div class="container5">
+        <font size="5px" class="container1title1">Ingrese valor $</font>
+        <br /><input
+          type="number"
+          id="CC_CAMPO1"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar solo el valor numerico</font
+        >
+      </div>
+      <div class="container6">
+        <font size="5px" class="container1title1"
+          >Ingrese el volumen corregido</font
+        >
+        <br /><input
+          type="number"
+          id="CC_CAMPO1"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar numeros
+        </font>
+      </div>
+    </div>
+    <div class="container7">
+      <label for="cbox2">Por favor indique si llego servicio de agua</label>
+      <input type="checkbox" id="cbox2" value="second_checkbox" />
+    </div>
+
+    <div class="containeragua">
+      <div class="container8">
+        <font size="10px" class="title1">Servicio de agua</font>
+      </div>
+      <div class="container9">
+        <font size="5px" class="container1title1">Ingrese valor $</font>
+        <!-- <label size="9px" for="CC_CAMPO1">Ingrese valor</label> -->
+        <br /><input
+          type="number"
+          id="CC_CAMPO1"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar solo el valor numerico</font
+        >
+      </div>
+      <div class="container10">
+        <font size="5px" class="container1title1"
+          >Ingrese el volumen corregido</font
+        >
+        <br /><input
+          type="number"
+          id="CC_CAMPO1"
+          name="CC_CAMPO1"
+          size="30 px"
+        />
+        <br />
+        <font size="3px" class="container1title2"
+          >*Nota: debe ingresar numeros
+        </font>
+      </div>
+    </div>
+  </body>
+</html>
+    `;
+    res.send(html);
+});
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+});
